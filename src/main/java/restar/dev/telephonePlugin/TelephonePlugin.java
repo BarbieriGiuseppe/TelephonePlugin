3ed69ada6d1b79864a091e6ebb981be823cae5a3
Giuseppe Barbieri <Giuseppe.Barbieri@wolterskluwer.com>
2025-01-08 15:35:36 +0100

    feat: update main method and playerlistener class

--- src/main/java/restar/dev/telephonePlugin/TelephonePlugin.java
+++ src/main/java/restar/dev/telephonePlugin/TelephonePlugin.java
@@ -1,22 +1,28 @@
 package restar.dev.telephonePlugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import restar.dev.telephonePlugin.listeners.PlayerListeners;
 import restar.dev.telephonePlugin.utils.PlayerFile;
+
+import java.util.logging.Logger;
 
 public final class TelephonePlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("Telephone plugin Enabled");
+        Bukkit.getConsoleSender().sendMessage("Telephone plugin Enabled");
 
         PlayerFile playerFile = new PlayerFile();
+        PlayerListeners playerListeners = new PlayerListeners(playerFile);
 
+        Bukkit.getPluginManager().registerEvents(playerListeners, this);
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Bukkit.getConsoleSender().sendMessage("Telephone plugin Disabled");
     }
 }
--- src/main/java/restar/dev/telephonePlugin/listeners/PlayerListeners.java
+++ src/main/java/restar/dev/telephonePlugin/listeners/PlayerListeners.java
@@ -1,5 +1,6 @@
 package restar.dev.telephonePlugin.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import restar.dev.telephonePlugin.utils.PlayerFile;
 
@@ -10,5 +11,17 @@ public class PlayerListeners implements Listener {
     public PlayerListeners(PlayerFile playerFile) {
 
         this.playerFile = playerFile;
+    }
+    
+    public void setPlayerFile(Player player){
+        String uuid = String.valueOf(player.getUniqueId());
+        String playerName = player.getName();
+
+        playerFile.setName(playerName);
+        playerFile.setUuid(uuid);
+
+        playerFile.createPlayerFile(uuid,playerName);
+
+
     }
 }
